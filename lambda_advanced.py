7ec777d21f9f182994d8e84ae3143d6bc544c77e
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2022-11-25 23:46:43 +0500

    Update lambda_advanced.py

--- lambda_advanced.py
+++ lambda_advanced.py
@@ -4,49 +4,6 @@ data = open('data.json').read()
 users = json.loads(data)
 
 
-def max_odd(l: list) -> int:
-    '''returns the maximum odd number in the list 'l' given as an argument. (you may use max, labmda)
-    
-    Args:
-        l (list): list of numbers
-        
-    Returns:
-        int: maximum odd number
-    '''
-
-
-def min_odd(l: list) -> int:
-    '''returns the minimum odd number in the list 'l' given as an argument. (you may use min, labmda)
-    
-    Args:
-        l (list): list of numbers
-        
-    Returns:
-        int: maximum odd number
-    '''
-
-
-def max_even(l: list) -> int:
-    '''returns the maximum even number in the list 'l' given as an argument. (you may use max, labmda)
-    
-    Args:
-        l (list): list of numbers
-        
-    Returns:
-        int: maximum even number
-    '''
-
-
-def min_even(l: list) -> int:
-    '''returns the minimum even number in the list 'l' given as an argument. (you may use min, labmda)
-    
-    Args:
-        l (list): list of numbers
-        
-    Returns:
-        int: maximum even number
-    '''
-
 
 def oldest_user(users: list[dict]) -> dict:
     '''returns the oldest user in the list 'users' given as an argument. (you may use max, labmda)
